19e5b3d17a9992b6c197fff6a8f088984b8ef937
Chaitanya134 <chaitanyagiri2@gmail.com>
2023-07-03 00:15:14 +0530

    Fix useRecall error

--- client/src/pages/InsightsPage.tsx
+++ client/src/pages/InsightsPage.tsx
@@ -1,10 +1,7 @@
 import Chat from "../components/Chat";
 import Insights from "../components/Insights";
-import { useRecall } from "../contexts/RecallProvider";
 
 const InsightsPage = () => {
-  //   const { fileNames, transcriptionLanguage } = useAudio();
-
   return (
     <main className="min-h-screen overflow-hidden">
       <div className="container mx-auto h-screen px-8 py-8">
@@ -12,12 +9,6 @@ const InsightsPage = () => {
           <Chat />
           <Insights />
         </div>
-        {/* {fileNames.map((filename) => (
-          <div className="text-white" key={filename}>
-            {filename}
-          </div>
-        ))}
-        {transcriptionLanguage} */}
       </div>
     </main>
   );
