0479bad9e16483ebedc15178c08e982761271e67
Chaitanya134 <chaitanyagiri2@gmail.com>
2023-07-02 12:00:00 +0530

    Upadated Loader

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,7 +1,12 @@
+import { ScaleLoader } from "react-spinners";
+
 const Loading = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center text-2xl text-white">
-      Loading...
+    <div className="flex h-full w-full items-center justify-center text-2xl text-white">
+      <div className="flex items-center gap-4">
+        <span>Analysing Audio</span>
+        <ScaleLoader color="#ffffff" height="20" speedMultiplier={1.3} />
+      </div>
     </div>
   );
 };
